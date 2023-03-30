709630ee20a871c2c5f206ae8e812b30cdc2aff8
ajnads <85207958+Arvin-Jay-Nadal@users.noreply.github.com>
2023-03-30 17:29:04 +0800

    Add files via upload

---
+++ src/components/Todo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+//todos props to display all the all the todos that are added in the TodoList.js
+// use className="todo-row" when displaying todos for design
+const Todo = ({ todos }) => {
+  return <>{/* display todos here */}</>;
+};
+
+export default Todo;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+//get the inputted data from the input tag by using the button onClick handleSubmit
+// use the handleChange to get the value in input tag
+// and use the props to pass the input data to TodoList on submit
+
+function TodoForm(props) {
+  const [input, setInput] = React.useState("");
+
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="todo-form">
+      <input placeholder="Add a todo" name="text" className="todo-input" />
+      <button type="submit" className="todo-button">
+        Add todo
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
